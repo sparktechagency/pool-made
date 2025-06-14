8bcee6063498cf1e3d065eb5132a9978512b2403
Tanvir islam <tanvirislam3912@gmail.com>
2025-06-14 14:37:22 +0600

    Change Password page

---
+++ src/app/(drawer)/change-password.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function ChangePassword() {
+  return (
+    <View>
+      <Text>ChangePassword</Text>
+    </View>
+  );
+}
