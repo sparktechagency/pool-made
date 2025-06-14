2d4c75ae1dd35619ec76bbecb8df098cba4b3192
Tanvir islam <tanvirislam3912@gmail.com>
2025-06-14 14:40:26 +0600

    ViewQuotes page

---
+++ src/app/(drawer)/(tabs)/view-quotes.tsx
@@ -0,0 +1,15 @@
+import Wrapper from "@/src/components/Wrapper";
+import React from "react";
+import { Text, View } from "react-native";
+
+const ViewQuotes = () => {
+  return (
+    <Wrapper>
+      <View>
+        <Text>ViewQuotes</Text>
+      </View>
+    </Wrapper>
+  );
+};
+
+export default ViewQuotes;
