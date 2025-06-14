b7a7a5c160054ebb78b1c8bdf52655fd88401f75
Tanvir islam <tanvirislam3912@gmail.com>
2025-06-14 14:43:00 +0600

    business-provider-auth layout

---
+++ src/app/auth/business-provider-auth/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+// import "react-native-reanimated";
+
+export default function _layout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
+}
