2568768069ef851b370d5817185c832e726da4e1
Tanvir islam <tanvirislam3912@gmail.com>
2025-06-14 14:37:36 +0600

    Privacy Policy page

---
+++ src/app/(drawer)/privacy-policy.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function PrivacyPolicy() {
+  return (
+    <View>
+      <Text>PrivacyPolicy</Text>
+    </View>
+  );
+}
+
+// export default PrivacyPolicy
