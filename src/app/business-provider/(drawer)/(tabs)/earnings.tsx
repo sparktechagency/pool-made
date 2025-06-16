e25fbb931c869aca9335612992419abaf4c54a2c
Tanvir islam <tanvirislam3912@gmail.com>
2025-06-16 14:34:38 +0600

    Get Quotes  pages

---
+++ src/app/business-provider/(drawer)/(tabs)/earnings.tsx
@@ -0,0 +1,17 @@
+import { IconsHome } from "@/assets/icons";
+import Wrapper from "@/src/components/Wrapper";
+import React from "react";
+import { Text, View } from "react-native";
+import { SvgXml } from "react-native-svg";
+
+const GetQuotes = () => {
+  return (
+    <Wrapper>
+      <View>
+        <Text></Text>
+      </View>
+    </Wrapper>
+  );
+};
+
+export default GetQuotes;
