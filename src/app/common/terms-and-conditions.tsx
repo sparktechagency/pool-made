5f8fa7865b9f39b0f4fbe4166df43ec6f29da980
Tanvir islam <tanvirislam3912@gmail.com>
2025-06-16 14:35:48 +0600

    Terms Conditions pages

---
+++ src/app/common/terms-and-conditions.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function TermsConditions() {
+  return (
+    <View>
+      <Text>TermsConditions</Text>
+    </View>
+  );
+}
