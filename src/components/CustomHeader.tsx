2b7692edb751c29ad0cbe343798ecb96c0d24681
Tanvir islam <tanvirislam3912@gmail.com>
2025-06-21 14:18:05 +0600

    CustomHeader ui

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -1,6 +1,7 @@
 import { IconsMenu, IconsMenuNotification } from "@/assets/icons";
 import { DrawerNavigationProp } from "@react-navigation/drawer";
 import { useNavigation } from "@react-navigation/native";
+import { router } from "expo-router";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SvgXml } from "react-native-svg";
 import tw from "../lib/tailwind";
@@ -29,18 +30,22 @@ export default function CustomHeader() {
 
       <View style={tw`flex-row items-center gap-2`}>
         <TouchableOpacity
-          style={tw`bg-[#ECF1F6] w-11 h-11 rounded-full items-center flex justify-center`}
+          style={tw`bg-[#ECF1F6] relative w-11 h-11 rounded-full items-center flex justify-center`}
         >
           <SvgXml xml={IconsMenuNotification} />
+          <View
+            style={tw`bg-red-600 absolute right-[13px] top-[11px]  w-2 h-2 rounded-full `}
+          ></View>
         </TouchableOpacity>
-        <View
+        <TouchableOpacity
           style={tw`bg-[#ECF1F6] h-12 w-12 rounded-full items-center flex justify-center`}
+          onPress={() => router.push("/business-provider/user-profile")}
         >
           <Image
             source={require(`@/assets/images/Ellipse.png`)}
             style={tw`w-10 h-10 rounded-full `}
           />
-        </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
