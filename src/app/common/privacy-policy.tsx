aa64662dc32853b7d99cbc1df6577d7d285d63e5
Tanvir islam <tanvirislam3912@gmail.com>
2025-06-21 14:16:36 +0600

    Privacy Policy

---
+++ src/app/common/privacy-policy.tsx
@@ -0,0 +1,74 @@
+import { IconsPP } from "@/assets/icons";
+import BackButton from "@/src/components/ui/BackButton";
+import Wrapper from "@/src/components/Wrapper";
+import tw from "@/src/lib/tailwind";
+import React from "react";
+import { Text, View } from "react-native";
+import { SvgXml } from "react-native-svg";
+
+export default function PrivacyPolicy() {
+  return (
+    <Wrapper>
+      <BackButton title="Back" />
+      <View style={tw`flex-1 mt-10`}>
+        <View style={tw`flex-row items-start gap-3`}>
+          {/* Icon */}
+          <SvgXml xml={IconsPP} />
+          {/* Text Content */}
+          <View style={tw`flex-col items-start gap-1`}>
+            <Text style={tw`text-2xl font-roboto-600  text-black`}>
+              Terms & Conditions
+            </Text>
+            <Text style={tw`text-sm font-roboto-400 text-sub_title_color`}>
+              Updated July 17, 2025
+            </Text>
+          </View>
+        </View>
+        <View style={tw`flex-col items-start gap-3 pt-10`}>
+          {/* one */}
+          <View style={tw`flex-col items-start gap-3`}>
+            {/* Icon */}
+            <Text style={tw`text-xl font-roboto-600  text-[#003B73]`}>
+              1. Terms & Conditions
+            </Text>
+            {/* Text Content */}
+            <Text style={tw`text-sm font-roboto-400 text-sub_title_color`}>
+              Lorem ipsum dolor sit amet consectetur. Volutpat purus nunc tellus
+              lorem adipiscing. Convallis at mi dictumst nulla amet. Ipsum
+              consequat vel donec ut amet ante semper. Amet tempus tellus
+              aliquam volutpat enim dolor tristique.
+            </Text>
+          </View>
+          {/* one */}
+          <View style={tw`flex-col items-start gap-3`}>
+            {/* Icon */}
+            <Text style={tw`text-xl font-roboto-600  text-[#003B73]`}>
+              2. Your account
+            </Text>
+            {/* Text Content */}
+            <Text style={tw`text-sm font-roboto-400 text-sub_title_color`}>
+              Lorem ipsum dolor sit amet consectetur. Volutpat purus nunc tellus
+              lorem adipiscing. Convallis at mi dictumst nulla amet. Ipsum
+              consequat vel donec ut amet ante semper. Amet tempus tellus
+              aliquam volutpat enim dolor tristique.
+            </Text>
+          </View>
+          {/* one */}
+          <View style={tw`flex-col items-start gap-3`}>
+            {/* Icon */}
+            <Text style={tw`text-xl font-roboto-600  text-[#003B73]`}>
+              3. Payment procedure
+            </Text>
+            {/* Text Content */}
+            <Text style={tw`text-sm font-roboto-400 text-sub_title_color`}>
+              Lorem ipsum dolor sit amet consectetur. Volutpat purus nunc tellus
+              lorem adipiscing. Convallis at mi dictumst nulla amet. Ipsum
+              consequat vel donec ut amet ante semper. Amet tempus tellus
+              aliquam volutpat enim dolor tristique.
+            </Text>
+          </View>
+        </View>
+      </View>
+    </Wrapper>
+  );
+}
