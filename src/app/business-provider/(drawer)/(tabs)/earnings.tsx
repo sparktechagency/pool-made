7ffc486126a358caba60aca55b1a30bc7882e200
Tanvir islam <tanvirislam3912@gmail.com>
2025-06-21 12:08:05 +0600

    earnig page

--- src/app/business-provider/(drawer)/(tabs)/earnings.tsx
+++ src/app/business-provider/(drawer)/(tabs)/earnings.tsx
@@ -1,17 +1,36 @@
-import { IconsHome } from "@/assets/icons";
+import TransactionView from "@/src/components/ui/TransactionView";
 import Wrapper from "@/src/components/Wrapper";
+import tw from "@/src/lib/tailwind";
 import React from "react";
-import { Text, View } from "react-native";
-import { SvgXml } from "react-native-svg";
+import { Image, Text, View } from "react-native";
 
-const GetQuotes = () => {
+const Earnings = () => {
   return (
     <Wrapper>
-      <View>
-        <Text></Text>
+      <View style={tw` flex-1`}>
+        <View style={tw` relative`}>
+          <Image
+            source={require("@/assets/images/Transaction-ViewI-Image.png")}
+            style={tw`w-full h-38 rounded-xl`}
+          />
+          <View
+            style={tw`absolute top-[25%] left-0 right-0 px-6 flex-col gap-2.5`}
+          >
+            <Text style={tw`text-2xl text-white font-roboto-600`}>
+              Transaction
+            </Text>
+            <Text style={tw`text-sm text-white font-roboto-500`}>
+              You have to wait for service owners can accept your bid request.
+            </Text>
+          </View>
+        </View>
+
+        <View style={tw` flex-1`}>
+          <TransactionView />
+        </View>
       </View>
     </Wrapper>
   );
 };
 
-export default GetQuotes;
+export default Earnings;
